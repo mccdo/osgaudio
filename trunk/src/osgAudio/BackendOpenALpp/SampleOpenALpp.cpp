4e77fdee0dc3387f6de20b5f5fc402c1ed431471
xenonofarcticus <xenonofarcticus@574e1c32-2fce-11df-82ed-5f3c927ac887>
2010-10-22 16:46:42 +0000

    Updating copyright text.

--- trunk/src/osgAudio/BackendOpenALpp/SampleOpenALpp.cpp
+++ trunk/src/osgAudio/BackendOpenALpp/SampleOpenALpp.cpp
@@ -1,7 +1,8 @@
 /* -*-c++-*- */
 /**
  * osgAudio - OpenSceneGraph Audio Library
- * Copyright (C) 2010 AlphaPixel, LLC
+ * (C) Copyright 2009-2011 by Kenneth Mark Bryden
+ * (programming by Chris 'Xenon' Hanson, AlphaPixel, LLC xenon at alphapixel.com)
  * based on a fork of:
  * Osg AL - OpenSceneGraph Audio Library
  * Copyright (C) 2004 VRlab, Umeå University
