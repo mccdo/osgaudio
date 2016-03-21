5aa33719c4b0c1db3980854f62a3746d6b333db8
F. Cami <fcami@fedoraproject.org>
2016-03-22 00:05:26 +0100

    fix the FSF's address to match COPYING

--- examples/osgaudio-lowlevel/moving.cpp
+++ examples/osgaudio-lowlevel/moving.cpp
@@ -20,7 +20,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #include <osgAudio/Source.h>
 #include <osgAudio/AudioEnvironment.h> // necessary for 3D spatial audio update()
